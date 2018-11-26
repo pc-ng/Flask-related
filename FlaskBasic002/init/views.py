99976cef9875c8aece5df691c273e2abd30713ca
pcngnervous <38524186+pc-ng@users.noreply.github.com>
2018-11-26 10:57:19 +0800

    Update 112618-1057

--- FlaskBasic002/init/views.py
+++ FlaskBasic002/init/views.py
@@ -1,6 +1,15 @@
+from datetime import datetime
 from flask import Flask
 from init import app
 
 @app.route('/')
 def home():
-    return "Hello Again!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    html_content = "<html><head><title>Hello Flask</title></head><body>"
+    html_content += "<strong>Hello Flask!</strong> on "
+    html_content += formatted_now
+    html_content += "</body></html>"
+
+    return html_content
