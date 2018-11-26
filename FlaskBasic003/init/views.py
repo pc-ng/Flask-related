b4e955cb63c034f8dd45547b580e0be4ea4bcbbe
pcngnervous <38524186+pc-ng@users.noreply.github.com>
2018-11-26 11:18:12 +0800

    update 261118-1118

---
+++ FlaskBasic003/init/views.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from init import app
+
+from datetime import datetime
+
+@app.route('/')
+def home():
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = " on " + formatted_now
+        )
